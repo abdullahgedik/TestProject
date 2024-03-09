f6b80bb68bfe3d80e0a27203575637544d3dabca
Abdullah Gedik <113609377+abdullahgedik@users.noreply.github.com>
2024-03-09 21:45:40 +0300

    Commit First

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        byte byteValue = 255;
+        short shortValue = 32767;
+        int intValue = 2147483647;
+        long longValue = 9223372036854775807L;
+
+        float floatValue = 3.14f;
+        double doubleValue = 3.141592653589793;
+
+        char charValue = 'A';
+
+        bool boolValue = true;
+
+        string stringValue = "Hello, World!";
+
+        object objectValue = 42;
+
+        Console.WriteLine($"Byte Value: {byteValue}");
+        Console.WriteLine($"Short Value: {shortValue}");
+        Console.WriteLine($"Int Value: {intValue}");
+        Console.WriteLine($"Long Value: {longValue}");
+        Console.WriteLine($"Float Value: {floatValue}");
+        Console.WriteLine($"Double Value: {doubleValue}");
+        Console.WriteLine($"Char Value: {charValue}");
+        Console.WriteLine($"Bool Value: {boolValue}");
+        Console.WriteLine($"String Value: {stringValue}");
+        Console.WriteLine($"Object Value: {objectValue}");
+    }
+}
