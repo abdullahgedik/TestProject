d8d0fd9e2677c52a13aab604c9d580bf6efdc328
Abdullah Gedik <113609377+abdullahgedik@users.noreply.github.com>
2024-03-11 22:09:47 +0300

    Update

--- obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b73c5aedc274908b2122186018097726ee3c4a0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8aa9154fa87301026dc72bc2e65172317b6c5bbb")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
