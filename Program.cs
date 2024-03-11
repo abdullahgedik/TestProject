8aa9154fa87301026dc72bc2e65172317b6c5bbb
Abdullah Gedik <113609377+abdullahgedik@users.noreply.github.com>
2024-03-11 19:35:14 +0300

    Switch - Case

--- Program.cs
+++ Program.cs
@@ -121,6 +121,24 @@ internal class Program
             Console.WriteLine(number);
         else
             Console.WriteLine(number);
+
+        //-------------------------------- Switch - Case -------------------------------------
+
+        int number; int.TryParse(Console.ReadLine(), out number);
+        switch(number)
+        {
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+
+            default:
+                break;
+        }
         */
     }
 }
